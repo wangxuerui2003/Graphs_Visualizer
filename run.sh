71f8f3967f3c80287c73b8c99e05458e12c05f42
wangxuerui <wangxuerui2003@gmail.com>
2022-09-29 21:46:27 +0800

    add checking of arguments

--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@ make
 
 mkdir -p results
 
-./graphing $1 | dot -Tpdf > results/$2.pdf
+if [ $# -eq 2 ]
+then
+	./graphing $1 | dot -Tpdf > results/$2.pdf
+else
+	echo "Usage: ./run.sh <number of nodes on the graph> <filename output>"
+fi
+
